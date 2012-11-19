b8a3d3b300a58606afdc48c09c2ed9c5faf47907
mrezzosoftware@gmail.com <mrezzosoftware@gmail.com@12023151-4e36-5912-7d31-9b68b7bbff33>
2012-11-19 21:43:36 +0000

    fuçando.

--- mmgp/index.php
+++ mmgp/index.php
@@ -1,17 +1,20 @@
 <?php
-
 $email = $_POST["email"];
 $dispositivo = $_POST["dispositivo"];
 $operacao = $_POST["operacao"];
 
-function emailCadastrado($mail) {
-    $QUERY = "SELECT * FROM usuarios WHERE email = \"" . $mail . "\"";
+$conn = mysql_connect("localhost", "mrsoftware", "MRS2012");
+$db = mysql_select_db("mrsoftware");
+
+function emailCadastrado($email) {
+    $QUERY = "SELECT * FROM usuarios WHERE email = '" . $email . "'";
     $result = mysql_query($QUERY);
     return mysql_num_rows($result) == 1 ? true : false;
 }
 
-function regitrarUsuario($mail, $senha) {
-    if (emailCadastrado($mail) == false) {
+function regitrarUsuario($email, $senha) {
+    if (emailCadastrado($email) == false) {
+        //$INSERT = "INSERT INTO usuarios VALUES ('" . $mail . "', '" . $senha . "')";
         echo "Usuário não cadastrado!";
     } else {
         echo "Usuário cadastrado";
@@ -23,79 +26,51 @@ function situacaoAtualMaquinas() {
 }
 
 function autenticarUsuario() {
+    
 }
-
 
 if ($email != null) {
 
     if ($dispositivo == "mobile") {
-        
+
         if ($operacao == "atualizarAcoesMaquina") {
             
+        } else if ($operacao == "obterAcoesMaquina") {
+
+            $maquina = $_POST["maquina"];
+            
+            $QUERY = "SELECT * FROM maquinas WHERE email = \"" . $email . "\" AND maquina = \"" . $maquina . "\"";
+            $result = mysql_query($QUERY);
+
+            if (mysql_num_rows($result) == 0) {
+                echo "";
+                return;
+            }
+
+            $operacoes = mysql_fetch_array($result);
+
+            echo $operacoes ['assinante'] .
+            ":" . $operacoes ['bloquear'] .
+            ":" . $operacoes ['logoff'] .
+            ":" . $operacoes ['hibernar'] .
+            ":" . $operacoes ['reiniciar'] .
+            ":" . $operacoes ['desligar'] .
+            ":" . $operacoes ['keylogger'] .
+            ":" . $operacoes ['geolocalizacao'] .
+            ":" . $operacoes ['latitude'] .
+            ":" . $operacoes ['longitude'];
         } else if ($operacao == "autenticarUsuario") {
             
-        } else if ($operacao == "registrarUsuario") {
-            
+        } else if ($operacao == "registrar_usuario") {
+            $senha = $_POST["pwd"];
+            regitrarUsuario($email, $senha);
         }
-
-        if (emailCadastrado($email) == false) {
-            echo "Não registrado";
-        } else {
-            echo "Registrado";
-        }
+        
     } else if ($dispositivo == "pc") {
         
     }
-}
 
-
-if ($email != null) {
-
-    $conn = mysql_connect("localhost", "mrsoftware", "MRS2012");
-    $db = mysql_select_db("mrsoftware");
-    $QUERY = "";
-
-    $verificarEmail = $_POST["verificarEmail"] == true ? true : false;
-
-    if ($verificarEmail == true) {
-        echo emailCadastrado($email);
-        mysql_close($con);
-        return;
-    }
-
-
-    $maquina = $_POST["verInformacoesMaquina"];
-
-    if ($maquina != null) {
-
-        $QUERY = "SELECT * FROM maquinas WHERE email = \"" . $email . "\" AND maquina = \"" . $maquina . "\"";
-        $result = mysql_query($QUERY);
-
-        if (mysql_num_rows($result) == 0) {
-            echo "";
-            return;
-        }
-
-        $operacoes = mysql_fetch_array($result);
-
-        echo $operacoes ['assinante'] .
-        ":" . $operacoes ['bloquear'] .
-        ":" . $operacoes ['logoff'] .
-        ":" . $operacoes ['hibernar'] .
-        ":" . $operacoes ['reiniciar'] .
-        ":" . $operacoes ['desligar'] .
-        ":" . $operacoes ['keylogger'] .
-        ":" . $operacoes ['geolocalizacao'] .
-        ":" . $operacoes ['latitude'] .
-        ":" . $operacoes ['longitude'];
-
-        mysql_close($con);
-        return;
-    }
-
-    mysql_close($con);
-} else {
-    echo "Email não informado.";
+    mysql_close();
 }
 ?>
 
