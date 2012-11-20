fe255613feddaa063e4bab4d8ec1670682b4de65
mrezzosoftware@gmail.com <mrezzosoftware@gmail.com@12023151-4e36-5912-7d31-9b68b7bbff33>
2012-11-20 22:57:34 +0000

    fuçando.

--- mmgp/index.php
+++ mmgp/index.php
@@ -10,7 +10,6 @@ if (!$conexao) {
     echo mysql_error() . "<br/>";
     exit();
 }
-
 $db = mysql_select_db("mrsoftware");
 
 function apresentarErroMySql() {
@@ -33,35 +32,40 @@ function regitrarUsuario($email, $senha) {
     }
 }
 
-function obterMaquinasUsuario($email) {
-    
-}
-
-function atualizarAcoesMaquinas($email, $maquina, $dados) {
-
-    list($bloquear, $logoff, $hibernar, $reiniciar, $desligar, $keylogger, $geolocalizacao, $latitude, $longitude) = explode(":", $dados);
-
-    $UPDATE = "
-        UPDATE maquinas
-        SET bloquear = '" . $bloquear . "',
-            logoff = '" . $logoff . "',
-            hibernar = '" . $hibernar . "',
-            reiniciar = '" . $reiniciar . "',
-            desligar = '" . $desligar . "',
-            keylogger = '" . $keylogger . "',
-            geolocalizacao = '" . $geolocalizacao . "'
-        WHERE email = '" . $email . "' AND maquina = '" . $maquina . "'";
-    echo $UPDATE . "<br/>";
-    $result = mysql_query($UPDATE);
-    echo (mysql_affected_rows() == 1) ? "ATU-ACOES-MAQ-SUC" : "ERRO: " . apresentarErroMySql();
-}
-
 function autenticarUsuario($email, $senha) {
     $QUERY = "SELECT COUNT(*) AS registrado FROM usuarios WHERE email = '" . $email . "' AND senha = '" . $senha . "'";
     $result = mysql_query($QUERY);
     $resposta = mysql_fetch_array($result);
 
     echo $resposta['registrado'] == 1 ? "AUTH" : "NAUTH";
+}
+
+function obterMaquinasUsuario($email) {
+    $QUERY = "SELECT maquina FROM maquinas WHERE email = '" . $email . "'";
+    $result = mysql_query($QUERY);
+    $maquinas = "";
+    while ($linha = mysql_fetch_array($result)) {
+        $maquinas .= ($linha['maquina'] . ":");
+    }
+    $maquinas = substr($maquinas, 0, -1);
+    echo $maquinas;
+}
+
+function atualizarAcoesMaquina($email, $maquina, $dados) {
+
+    list($tempoAtualizacao, $acao, $keylogger, $geolocalizacao) = explode(":", $dados);
+
+    $UPDATE = "
+        UPDATE maquinas
+        SET tempo_atualizacao = '" . $tempoAtualizacao . "',
+            ultima_atualizacao = '" . date("Y-m-d H:i:s") . "',
+            acao = '" . $acao . "',
+            keylogger = '" . $keylogger . "',
+            geolocalizacao = '" . $geolocalizacao . "'
+        WHERE email = '" . $email . "' AND maquina = '" . $maquina . "'";
+    echo "UPDATE: " . $UPDATE;
+    $result = mysql_query($UPDATE);
+    echo (mysql_affected_rows() == 1) ? "ATU-ACOES-MAQ-SUC" : "ERRO: " . apresentarErroMySql();
 }
 
 function obterAcoesMaquina($email, $maquina) {
@@ -76,16 +80,21 @@ function obterAcoesMaquina($email, $maquina) {
 
     $operacoes = mysql_fetch_array($result);
 
-    echo $operacoes ['assinante'] .
-    ":" . $operacoes ['bloquear'] .
-    ":" . $operacoes ['logoff'] .
-    ":" . $operacoes ['hibernar'] .
-    ":" . $operacoes ['reiniciar'] .
-    ":" . $operacoes ['desligar'] .
+    echo $operacoes ['ligada'] .
+    ":" . $operacoes ['tempo_atualizacao'] .
+    ":" . $operacoes ['ultima_atualizacao'] .
+    ":" . $operacoes ['acao'] .
     ":" . $operacoes ['keylogger'] .
     ":" . $operacoes ['geolocalizacao'] .
     ":" . $operacoes ['latitude'] .
     ":" . $operacoes ['longitude'];
+}
+
+function atualizarSituacaoMaquina($email, $maquina, $situacao) {
+
+    $UPDATE = "UPDATE maquinas SET ligada = " . $situacao . ", ultima_atualizacao= '" . date("Y-m-d H:i:s") . "' WHERE email = '" . $email . "' AND maquina = '" . $maquina . "'";
+    $result = mysql_query($UPDATE);
+    echo (mysql_affected_rows() == 1) ? "ATU-SIT-MAQ-SUC" : "ERRO: " . apresentarErroMySql();
 }
 
 if ($email != null) {
@@ -95,23 +104,37 @@ if ($email != null) {
 
             $maquina = $_POST["maquina"];
             $dados = $_POST["dados"];
-            atualizarAcoesMaquinas($email, $maquina, $dados);
-            
-        } else if ($operacao == "obterAcoesMaquina") {
-
-            $maquina = $_POST["maquina"];
-            obterAcoesMaquina($email, $maquina);
+            atualizarAcoesMaquina($email, $maquina, $dados);
         } else if ($operacao == "autenticarUsuario") {
 
             $senha = $_POST["pwd"];
             autenticarUsuario($email, $senha);
+        } else if ($operacao == "obterMaquinasUsuario") {
+
+            obterMaquinasUsuario($email);
+        } else if ($operacao == "obterAcoesMaquina") {
+
+            $maquina = $_POST["maquina"];
+            obterAcoesMaquina($email, $maquina);
         } else if ($operacao == "registrarUsuario") {
 
             $senha = $_POST["pwd"];
             regitrarUsuario($email, $senha);
+        } else if ($operacao == "tst") {
+            echo date("Y-m-d H:i:s"). " timestamp";
         }
     } else if ($dispositivo == "pc") {
-        
+        if ($operacao == "obterAcoesMaquina") {
+
+            $maquina = $_POST["maquina"];
+            obterAcoesMaquina($email, $maquina);
+            atualizarSituacaoMaquina($email, $maquina, 1);
+        }else if ($operacao == "atualizarSituacaoMaquina") {
+
+            $maquina = $_POST["maquina"];
+            $situacao = $_POST["situacao"];
+            atualizarSituacaoMaquina($email, $maquina, $situacao);
+        }
     }
 
     mysql_close();
