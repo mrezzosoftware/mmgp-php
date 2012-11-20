c943c491bcac4add07af506acee126113dda5481
mrezzosoftware@gmail.com <mrezzosoftware@gmail.com@12023151-4e36-5912-7d31-9b68b7bbff33>
2012-11-20 03:35:44 +0000

    Reorganizando o funcionamento

--- mmgp/index.php
+++ mmgp/index.php
@@ -3,8 +3,19 @@ $email = $_POST["email"];
 $dispositivo = $_POST["dispositivo"];
 $operacao = $_POST["operacao"];
 
-$conn = mysql_connect("localhost", "mrsoftware", "MRS2012");
+$conexao = mysql_connect("localhost", "mrsoftware", "MRS2012");
+if (!$conexao) {
+    echo "ERRO-CONEXAO<br/>";
+    echo mysql_errno() . "<br/>";
+    echo mysql_error() . "<br/>";
+    exit();
+}
+
 $db = mysql_select_db("mrsoftware");
+
+function apresentarErroMySql() {
+    return mysql_errno() . " - " . mysql_error();
+}
 
 function emailCadastrado($email) {
     $QUERY = "SELECT * FROM usuarios WHERE email = '" . $email . "'";
@@ -14,58 +25,91 @@ function emailCadastrado($email) {
 
 function regitrarUsuario($email, $senha) {
     if (emailCadastrado($email) == false) {
-        //$INSERT = "INSERT INTO usuarios VALUES ('" . $mail . "', '" . $senha . "')";
-        echo "Usuário não cadastrado!";
+        $INSERT = "INSERT INTO usuarios VALUES ('" . $email . "', '" . $senha . "')";
+        $result = mysql_query($INSERT);
+        echo (mysql_affected_rows() == 1) ? "CAD-SUC" : "ERRO: " . apresentarErroMySql();
     } else {
-        echo "Usuário cadastrado";
+        echo "JA-CAD";
     }
 }
 
-function situacaoAtualMaquinas() {
+function obterMaquinasUsuario($email) {
     
 }
 
-function autenticarUsuario() {
-    
+function atualizarAcoesMaquinas($email, $maquina, $dados) {
+
+    list($bloquear, $logoff, $hibernar, $reiniciar, $desligar, $keylogger, $geolocalizacao, $latitude, $longitude) = explode(":", $dados);
+
+    $UPDATE = "
+        UPDATE maquinas
+        SET bloquear = '" . $bloquear . "',
+            logoff = '" . $logoff . "',
+            hibernar = '" . $hibernar . "',
+            reiniciar = '" . $reiniciar . "',
+            desligar = '" . $desligar . "',
+            keylogger = '" . $keylogger . "',
+            geolocalizacao = '" . $geolocalizacao . "'
+        WHERE email = '" . $email . "' AND maquina = '" . $maquina . "'";
+    echo $UPDATE . "<br/>";
+    $result = mysql_query($UPDATE);
+    echo (mysql_affected_rows() == 1) ? "ATU-ACOES-MAQ-SUC" : "ERRO: " . apresentarErroMySql();
+}
+
+function autenticarUsuario($email, $senha) {
+    $QUERY = "SELECT COUNT(*) AS registrado FROM usuarios WHERE email = '" . $email . "' AND senha = '" . $senha . "'";
+    $result = mysql_query($QUERY);
+    $resposta = mysql_fetch_array($result);
+
+    echo $resposta['registrado'] == 1 ? "AUTH" : "NAUTH";
+}
+
+function obterAcoesMaquina($email, $maquina) {
+
+    $QUERY = "SELECT * FROM maquinas WHERE email = \"" . $email . "\" AND maquina = \"" . $maquina . "\"";
+    $result = mysql_query($QUERY);
+
+    if (mysql_num_rows($result) == 0) {
+        echo "";
+        return;
+    }
+
+    $operacoes = mysql_fetch_array($result);
+
+    echo $operacoes ['assinante'] .
+    ":" . $operacoes ['bloquear'] .
+    ":" . $operacoes ['logoff'] .
+    ":" . $operacoes ['hibernar'] .
+    ":" . $operacoes ['reiniciar'] .
+    ":" . $operacoes ['desligar'] .
+    ":" . $operacoes ['keylogger'] .
+    ":" . $operacoes ['geolocalizacao'] .
+    ":" . $operacoes ['latitude'] .
+    ":" . $operacoes ['longitude'];
 }
 
 if ($email != null) {
-
     if ($dispositivo == "mobile") {
 
         if ($operacao == "atualizarAcoesMaquina") {
+
+            $maquina = $_POST["maquina"];
+            $dados = $_POST["dados"];
+            atualizarAcoesMaquinas($email, $maquina, $dados);
             
         } else if ($operacao == "obterAcoesMaquina") {
 
             $maquina = $_POST["maquina"];
-            
-            $QUERY = "SELECT * FROM maquinas WHERE email = \"" . $email . "\" AND maquina = \"" . $maquina . "\"";
-            $result = mysql_query($QUERY);
-
-            if (mysql_num_rows($result) == 0) {
-                echo "";
-                return;
-            }
-
-            $operacoes = mysql_fetch_array($result);
-
-            echo $operacoes ['assinante'] .
-            ":" . $operacoes ['bloquear'] .
-            ":" . $operacoes ['logoff'] .
-            ":" . $operacoes ['hibernar'] .
-            ":" . $operacoes ['reiniciar'] .
-            ":" . $operacoes ['desligar'] .
-            ":" . $operacoes ['keylogger'] .
-            ":" . $operacoes ['geolocalizacao'] .
-            ":" . $operacoes ['latitude'] .
-            ":" . $operacoes ['longitude'];
+            obterAcoesMaquina($email, $maquina);
         } else if ($operacao == "autenticarUsuario") {
-            
-        } else if ($operacao == "registrar_usuario") {
+
+            $senha = $_POST["pwd"];
+            autenticarUsuario($email, $senha);
+        } else if ($operacao == "registrarUsuario") {
+
             $senha = $_POST["pwd"];
             regitrarUsuario($email, $senha);
         }
-        
     } else if ($dispositivo == "pc") {
         
     }
