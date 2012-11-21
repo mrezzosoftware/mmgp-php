33e05d7612aa4644fbed7ed501def93e03ef7157
mrezzosoftware@gmail.com <mrezzosoftware@gmail.com@12023151-4e36-5912-7d31-9b68b7bbff33>
2012-11-21 01:57:46 +0000

    Funções básicas mobile e pc prontas

--- mmgp/index.php
+++ mmgp/index.php
@@ -22,11 +22,16 @@ function emailCadastrado($email) {
     return mysql_num_rows($result) == 1 ? true : false;
 }
 
+/**
+ *
+ * Funcionalidades utilizadas pelo programa do PC
+ *  
+ */
 function regitrarUsuario($email, $senha) {
     if (emailCadastrado($email) == false) {
         $INSERT = "INSERT INTO usuarios VALUES ('" . $email . "', '" . $senha . "')";
-        $result = mysql_query($INSERT);
-        echo (mysql_affected_rows() == 1) ? "CAD-SUC" : "ERRO: " . apresentarErroMySql();
+        mysql_query($INSERT);
+        echo (mysql_affected_rows() == 1) ? "CAD-USU-SUC" : "ERRO: " . apresentarErroMySql();
     } else {
         echo "JA-CAD";
     }
@@ -64,7 +69,7 @@ function atualizarAcoesMaquina($email, $maquina, $dados) {
             geolocalizacao = '" . $geolocalizacao . "'
         WHERE email = '" . $email . "' AND maquina = '" . $maquina . "'";
     echo "UPDATE: " . $UPDATE;
-    $result = mysql_query($UPDATE);
+    mysql_query($UPDATE);
     echo (mysql_affected_rows() == 1) ? "ATU-ACOES-MAQ-SUC" : "ERRO: " . apresentarErroMySql();
 }
 
@@ -88,6 +93,33 @@ function obterAcoesMaquina($email, $maquina) {
     ":" . $operacoes ['geolocalizacao'] .
     ":" . $operacoes ['latitude'] .
     ":" . $operacoes ['longitude'];
+}
+
+/**
+ *
+ * Funcionalidades utilizadas pelo programa do PC
+ *  
+ */
+function maquinaCadastrada($email, $maquina) {
+
+    $QUERY = "SELECT * FROM maquinas WHERE email = '" . $email . "' AND maquina = '" . $maquina . "'";
+    $result = mysql_query($QUERY);
+    return mysql_num_rows($result) == 1 ? true : false;
+}
+
+function cadastrarMaquina($email, $maquina) {
+
+    if (emailCadastrado($email) == true) {
+        if (maquinaCadastrada($email, $maquina) == false) {
+            $INSERT = "INSERT INTO maquinas (email, maquina) VALUES ('" . $email . "', '" . $maquina . "')";
+            mysql_query($INSERT);
+            echo (mysql_affected_rows() == 1) ? "CAD-MAQ-SUC" : "ERRO: " . apresentarErroMySql();
+        } else {
+            echo "MAQ-JA-CAD";
+        }
+    } else {
+        echo "MAIL-NAO-CAD";
+    }
 }
 
 function atualizarSituacaoMaquina($email, $maquina, $situacao) {
@@ -121,7 +153,7 @@ if ($email != null) {
             $senha = $_POST["pwd"];
             regitrarUsuario($email, $senha);
         } else if ($operacao == "tst") {
-            echo date("Y-m-d H:i:s"). " timestamp";
+            echo date("Y-m-d H:i:s") . " timestamp";
         }
     } else if ($dispositivo == "pc") {
         if ($operacao == "obterAcoesMaquina") {
@@ -129,11 +161,15 @@ if ($email != null) {
             $maquina = $_POST["maquina"];
             obterAcoesMaquina($email, $maquina);
             atualizarSituacaoMaquina($email, $maquina, 1);
-        }else if ($operacao == "atualizarSituacaoMaquina") {
+        } else if ($operacao == "atualizarSituacaoMaquina") {
 
             $maquina = $_POST["maquina"];
             $situacao = $_POST["situacao"];
             atualizarSituacaoMaquina($email, $maquina, $situacao);
+        } else if ($operacao == "cadastrarMaquina") {
+
+            $maquina = $_POST["maquina"];
+            cadastrarMaquina($email, $maquina);
         }
     }
 
